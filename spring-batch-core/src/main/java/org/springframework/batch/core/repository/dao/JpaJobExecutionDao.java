d8448224b35b6dbb708f19790729d6734411948f
paranoiabla <paranoiabla@gmail.com>
2014-02-20 14:40:18 -0800

    adding manifest files

--- spring-batch-core/src/main/java/org/springframework/batch/core/repository/dao/JpaJobExecutionDao.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.batch.core.repository.dao;
-
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JpaJobExecutionDao<T> extends PagingAndSortingRepository<T, Long>, JobExecutionDao, InitializingBean {
-}
